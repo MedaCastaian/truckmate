86c90f4d174c23c9b2d501aeb7b293bad04a2858
Meda-Liliana Decean <meda.decean@student.upt.ro>
2025-10-01 15:41:09 +0300

    Reorganize project into Maven src/main/java structure

---
+++ src/main/java/com/truckmate/truckmate/TruckmateApplication.java
@@ -0,0 +1,11 @@
+package com.truckmate.truckmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckmateApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TruckmateApplication.class, args);
+  }
+}
